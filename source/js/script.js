e2ab1e07284420fe0bf7b701cbbb55590ca80423
Заур <zaurinio@inbox.ru>
2021-09-10 01:29:16 +0300

    правки по замечаниям наставника

--- source/js/script.js
+++ source/js/script.js
@@ -4,14 +4,14 @@ var navToggle = document.querySelector(".page-header__toggle");
 navMain.classList.remove("main-navigation--nojs");
 navToggle.addEventListener("click", function() {
 if (navMain.classList.contains("main-navigation--closed")) {
-    navMain.classList.remove("main-navigation--closed");
-    navToggle.classList.remove("page-header__toggle--closed");
-    navMain.classList.add("main-navigation--opened");
-    navToggle.classList.add("page-header__toggle--opened");
+  navMain.classList.remove("main-navigation--closed");
+  navToggle.classList.remove("page-header__toggle--closed");
+  navMain.classList.add("main-navigation--opened");
+  navToggle.classList.add("page-header__toggle--opened");
 } else {
-    navMain.classList.add("main-navigation--closed");
-    navMain.classList.remove("main-navigation--opened");
-    navToggle.classList.add("page-header__toggle--closed");
-    navToggle.classList.remove("page-header__toggle--opened");
-}
+  navMain.classList.add("main-navigation--closed");
+  navMain.classList.remove("main-navigation--opened");
+  navToggle.classList.add("page-header__toggle--closed");
+  navToggle.classList.remove("page-header__toggle--opened");
+  }
 })
